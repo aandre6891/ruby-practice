35cbde49c2ef1647e300fc71f664e05db1ecf90f
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-03-20 13:14:56 +0000

    Dice game 2 players correct

--- dice.rb
+++ dice.rb
@@ -1,15 +1,15 @@
-random_number1 = rand(6)
-random_number2 = rand(6)
+random_number1 = rand(1..6)
+random_number2 = rand(1..6)
 
 if random_number1 > random_number2
-  puts "Player 1: " random_number1 "- Player 2: " random_number2
+  puts "Player 1: #{random_number1} - Player 2:  #{random_number2}"
   puts "Player 1 wins!"
-elseif
+elsif
   random_number1 < random_number2
-  puts "Player 1: " random_number1 "- Player 2: " random_number2
+  puts "Player 1: #{random_number1} - Player 2:  #{random_number2}"
   puts "Player 2 wins!"
 else
-  puts "Player 1: " random_number1 "- Player 2: " random_number2
+  puts "Player 1: #{random_number1} - Player 2:  #{random_number2}"
   puts "Draw!"
   
 end
