9c76dc0f86200fc67a719efbad7a71188e6b56c4
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-03-20 13:10:19 +0000

    Dice Game between two players

--- dice.rb
+++ dice.rb
@@ -1,7 +1,15 @@
-random_number = rand(10)
+random_number1 = rand(6)
+random_number2 = rand(6)
 
-if random_number < 5
-  return 0
+if random_number1 > random_number2
+  puts "Player 1: " random_number1 "- Player 2: " random_number2
+  puts "Player 1 wins!"
+elseif
+  random_number1 < random_number2
+  puts "Player 1: " random_number1 "- Player 2: " random_number2
+  puts "Player 2 wins!"
 else
-  return 10
+  puts "Player 1: " random_number1 "- Player 2: " random_number2
+  puts "Draw!"
+  
 end
